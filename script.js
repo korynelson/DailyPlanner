51555057bf4abb973a866e23a3d92e1c96e05f4e
Kory Nelson <kory.nelson99@gmail.com>
2020-09-13 02:02:22 -0700

    added renderEvent function.  Also added time update for every second. also added save to local storage on clicking save

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function updateTime () {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, HH:mm:ss A'));
+};
+
+function renderEvents(){
+    $("textarea").each(function(){
+        var eventTime = $(this).siblings("div.hour").text();
+        $(this).text(window.localStorage.getItem(eventTime));
+    })
+}
+
 $(document).ready(function(){
-    $("#currentDay").text(moment().format('MMMM Do, YYYY'));
+    updateTime();
+    setInterval(updateTime, 1000);
+    renderEvents()
+
+    $(".saveBtn").on("click", function(){
+        var testing = $(this).siblings("div.hour").text()
+        window.localStorage.setItem(testing,$(this).siblings("textarea").val());
+    })
+    
 })
